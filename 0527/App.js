4f7e9595733adbc8d2dbed1659b088ec2c795d5c
RROOoyYy <51128694+RROOoyYy@users.noreply.github.com>
2021-06-03 22:10:51 +0800

    create 0527 app.js
    add button

---
+++ 0527/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@material-ui/core/Button'
+import SaveIcon from '@material-ui/icons/Save';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <ButtonGroup variant = "contained" size = "large" color="primary">
+              <Button startIcon = {<SaveIcon />}>
+                Save
+              </Button>
+              <Button startIcon = {<DeleteIcon />}>
+                Discard
+              </Button>
+          </ButtonGroup>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
